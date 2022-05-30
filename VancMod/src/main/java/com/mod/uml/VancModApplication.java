eaff7de8e368629959cdad13231aa40e92782e8c
Vanessa Cunha Carraro <vandyyh@gmail.com>
2022-05-30 15:22:47 -0300

    at7 instânciação

--- VancMod/src/main/java/com/mod/uml/VancModApplication.java
+++ VancMod/src/main/java/com/mod/uml/VancModApplication.java
@@ -1,13 +1,30 @@
 package com.mod.uml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class VancModApplication {
+import com.mod.uml.domain.Categoria;
+import com.mod.uml.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class VancModApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(VancModApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
