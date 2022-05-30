a2e48856f5ebb8506e83ccfd5dffb02f85b49871
Vanessa Cunha Carraro <vandyyh@gmail.com>
2022-05-30 13:40:22 -0300

    at2 Criando Projeto

---
+++ VancMod/src/main/java/com/mod/uml/VancModApplication.java
@@ -0,0 +1,13 @@
+package com.mod.uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VancModApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VancModApplication.class, args);
+	}
+
+}
